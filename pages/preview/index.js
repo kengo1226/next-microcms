2c153d67a77f7158bdd1ad9c1fc1c402c9cf3da0
岩本 <k_iwamoto@stride-web.co.jp>
2023-03-03 20:28:15 +0900

    プレビュー修正

--- pages/preview/index.js
+++ pages/preview/index.js
@@ -1,8 +1,27 @@
 import React, {useState, useEffect} from "react";
 import {useRouter} from "next/router";
 import { client } from "../../lib/Clients";
+import { siteTitle } from "../../components/layout";
+import { getAllPostIds, getPostData } from "../../lib/Posts";
 
-const NewsPage = () => {
+export async function getStaticPaths() {
+    const paths = await getAllPostIds();
+        return {
+            paths,
+            fallback: true,
+        };
+}
+
+export async function getStaticProps({ params }) {
+    const postData = await getPostData(params.id);
+    return {
+        props: {
+            postData,
+        },
+    };
+}
+
+export default function NewsPage () {
     const [data, setData] = useState();
     const router = useRouter();
     useEffect(() => {
@@ -22,12 +41,16 @@ const NewsPage = () => {
     return (
         <div>
             <h1>{data.title}</h1>
-            <p>{data.createdAt}</p>
+            <h1>{data.content}</h1>
         </div>
-    );
+    )
+    // return (
+    //     <div>
+    //         <h1>{data.title}</h1>
+    //         <p>{data.createdAt}</p>
+    //     </div>
+    // );
 }
-
-export default NewsPage
 
 // export default function previewHome() {
 //     console.log(data);
