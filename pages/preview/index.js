0ff9a029fc265707d103295b759aa69b2b360cf6
岩本 <k_iwamoto@stride-web.co.jp>
2023-03-03 15:06:26 +0900

    プレビューページの作成

---
+++ pages/preview/index.js
@@ -0,0 +1,27 @@
+import React, {useState, useEffect} from "react";
+import {useRouter} from "next/router";
+import { client } from "../../lib/Clients";
+
+export default function NewsPage() {
+    const [data, setData] = useState([]);
+    const router = useRouter();
+    useEffect(() => {
+        const getBlogsData = async () => {
+            if(!router.isReady) return;
+            const res = await client.get({
+                endpoint: 'blogs',
+                contentId: router.query.slug,
+                queries: { draftKey: router.query.draftKey },
+            });
+            setData(res);
+        };
+        getBlogsData();
+    }, [router]);
+
+    return (
+        <div>
+            <h1>data.title</h1>
+            <p>data.publishedAt</p>
+        </div>
+    );
+}
